60910fe3c8520c9f865d7921af672a08f91c1ffc
kli2m <ivanklimm87@gmail.com>
2020-03-07 08:19:32 +0300

    feat: added task createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) return false;
+    return members.filter(v => typeof(v) === "string").map(v => v.trim().toUpperCase()).map(v => v[0]).sort().join("");
 };
