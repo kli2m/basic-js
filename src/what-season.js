9468ef32e0d7237d7786d435289c6f90b0adf2e5
kli2m <ivanklimm87@gmail.com>
2020-03-12 22:18:59 +0300

    feat: task getSeason finished

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,8 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === null || date === undefined) return "Unable to determine the time of year!"
+    if (!date.getTime()) throw Error("throws an error on tricky moment")
+    if (Number.isNaN(Date.parse(date))) throw Error("throws an error on tricky moment")
+    const month = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"]
+    let d = new Date(date)
+    return month[d.getMonth()]
 };
