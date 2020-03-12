bb38f2e898a42d3a95fe6ba9cd55c012ce0c5b63
kli2m <ivanklimm87@gmail.com>
2020-03-12 22:07:58 +0300

    feat: task simple chain finished

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,29 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    arr: [],
+    getLength() {
+        return this.arr.length
+    },
+    addLink(value) {
+        this.arr.push(value)
+        return this
+    },
+    removeLink(position) {
+        if (typeof(position) !== 'number' || arguments.length === 0 || position <= 0 || (this.arr.length - 1) < position) {
+            this.arr = []
+            throw Error("throws an Error on removing wrong link")
+        }
+        this.arr.splice(position - 1, 1)
+        return this
+    },
+    reverseChain() {
+        this.arr.reverse()
+        return this
+    },
+    finishChain() {
+        let temp = this.arr.map(v => `( ${v} )`).join("~~")
+        this.arr = []
+        return temp
+    }
 };
 
 module.exports = chainMaker;
